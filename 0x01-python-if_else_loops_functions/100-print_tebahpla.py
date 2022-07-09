b7683697188241dd0a443a6693a65074e0ca6026
mishasharotakala <103355881+mishasharotakala@users.noreply.github.com>
2022-07-09 10:57:11 +0200

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+for letter in range(122, 96, -1):
+    if letter % 2 != 0:
+        letter = letter - 32
+    print("{:c}".format(letter), end='')
