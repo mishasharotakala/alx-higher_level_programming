3690fece29b8960fd7c444ec45a8ee4eb9151c40
mishasharotakala <103355881+mishasharotakala@users.noreply.github.com>
2022-07-09 10:58:50 +0200

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(i+1, 10):
-        if (i + j != 17):
-            print("{:d}{:d}".format(i, j), end=', ')
+for i in range(0, 9):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end='')
+        if i == 8 and j == 9:
+            print("")
         else:
-            print("{:d}{:d}".format(i, j))
+            print(", ", end='')
