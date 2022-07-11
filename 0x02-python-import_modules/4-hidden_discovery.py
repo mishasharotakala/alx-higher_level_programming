3bec615c2e1ccbd1041bfcd45ae51d4b3b8ea501
mishasharotakala <103355881+mishasharotakala@users.noreply.github.com>
2022-07-11 20:12:51 +0200

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
 if __name__ == "__main__":
-    words = dir(hidden_4)
-    for i in range(len(words)):
-        if words[i][0] != "_" and words[i][1] != "_":
-            print("{:s}".format(words[i]))
+
+    import hidden_4
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
