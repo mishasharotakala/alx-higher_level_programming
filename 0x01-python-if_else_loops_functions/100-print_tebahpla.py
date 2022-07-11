474fb78dc535807b203b7f896be6e44ffcfaa043
mishasharotakala <103355881+mishasharotakala@users.noreply.github.com>
2022-07-11 19:42:45 +0200

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for letter in range(122, 96, -1):
-    if letter % 2 != 0:
-        letter = letter - 32
-    print("{:c}".format(letter), end='')
+
+""""Print the alphabet in reverse order alternating upper- and lower-case."""
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
