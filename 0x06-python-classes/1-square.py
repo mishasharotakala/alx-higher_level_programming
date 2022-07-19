3cdfa41245c21ee5bc4d2794f909012b05abe19f
mishasharotakala <103355881+mishasharotakala@users.noreply.github.com>
2022-07-19 07:57:15 +0200

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a square class with a prvate size field"""
+
+
+class Square:
+    """private size field"""
+    def __init__(self, size):
+        """initializing the square"""
+        self.__size = size
