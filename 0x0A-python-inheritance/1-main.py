8c3d6b47f59cddc89fccfc3ade3ce99f88d02479
mishasharotakala <103355881+mishasharotakala@users.noreply.github.com>
2022-08-01 10:28:13 +0200

    Update 1-main.py

--- 0x0A-python-inheritance/1-main.py
+++ 0x0A-python-inheritance/1-main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""
-=============================
-Module with the method lookpu
-=============================
-"""
+MyList = __import__('1-my_list').MyList
 
-
-def lookup(obj):
-    """Function for return the attributes for an object"""
-
-    return dir(obj)
+my_list = MyList()
+my_list.append(1)
+my_list.append(4)
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+print(my_list)
+my_list.print_sorted()
+print(my_list)
