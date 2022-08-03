9c1d028a4054c7b224dde06f58bcc473a38cb1a6
mishasharotakala <103355881+mishasharotakala@users.noreply.github.com>
2022-08-03 07:53:03 +0200

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-Module 8-rectangle
+Module 9-rectangle
 Contains parent class BaseGeometry
-with public instance method area and integer_validator
+with public instance method area and integer_validation
 Contains subclass Rectangle
-with instantiation of private attributes width and height, validated by parent
+with instantiation of private attributes width and height, validated by parent,
+extends parent's area method and prints with __str__
 """
 
 
@@ -15,6 +16,8 @@ class Rectangle(BaseGeometry):
     """inherits from BaseGeometry
     Methods:
         __init__(self, width, height)
+        area(self)
+        __str__
     """
     def __init__(self, width, height):
         """validate and initialize width and height
@@ -22,7 +25,16 @@ class Rectangle(BaseGeometry):
             width (int): private
             height (int): private
         """
-        super().integer_validator("width", width)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
-        super().integer_validator("height", height)
         self.__height = height
+
+    def area(self):
+        """extends parent's empty method and returns area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """prints [Rectangle] <width>/<height>"""
+        return "[{:s}] {:d}/{:d}".format(self.__class__.__name__,
+                                         self.__width, self.__height)
