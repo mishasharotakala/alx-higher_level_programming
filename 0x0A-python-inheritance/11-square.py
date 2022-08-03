06cb145932a593fd21eb0373139da3c9b1487fb9
mishasharotakala <103355881+mishasharotakala@users.noreply.github.com>
2022-08-03 07:47:17 +0200

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
+"""
+Module 10-square
+Contains parent class BaseGeometry
+with public instance method area and integer_validation
+Contains subclass Rectangle
+with instantiation of private attributes width and height, validated by parent,
+extends parent's area method and prints with __str__
+Contains subclass Square
+with instantiation of private attribute size, validated by superclass,
+and prints with __str__
+"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
-"""
-===================================
-module with class BaseGeometry
-===================================
-"""
 
 
 class Square(Rectangle):
-    """Square class that inherits from Rectangle that inherits BaseGeometry"""
-
+    """inherits from Rectangle, who inherits from BaseGeometry
+    methods:
+        __init__(self, size)
+        __str__(self)
+    """
     def __init__(self, size):
-        """Method for initialized the attrubutes"""
-
-        super().__init__(size, size)
+        """initializes size
+        Args:
+            size (int): private
+        """
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
-    def area(self):
-        """rectangle area"""
-
-        return self.__size ** 2
-
     def __str__(self):
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        """prints [Rectangle] <width>/<height>"""
+        return "[{:s}] {:d}/{:d}".format(self.__class__.__name__,
+                                         self.__size, self.__size)
