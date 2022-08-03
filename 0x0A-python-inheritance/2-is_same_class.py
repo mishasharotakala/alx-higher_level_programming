f226459946e9e32048c8db1671aa8b10865bdc60
mishasharotakala <103355881+mishasharotakala@users.noreply.github.com>
2022-08-03 07:47:37 +0200

    Update 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """
-================================
-module with method is_same_class
-================================
+Module 2-is_same_class
+Contains method is_same_class
+returns True if object is exactly an instance of specified class
 """
 
 
 def is_same_class(obj, a_class):
-    """Method that return True if an object is an instance of a class"""
-
-    return type(obj) is a_class
+    """
+    Notes:
+        use type() to get specific class
+        use isinstance() to get class and any parent classes too
+        use issubclass() to get what object is a subclass of
+    Return:
+        True if obj is exactly an instance of specified class
+    """
+    return type(obj) == a_class
